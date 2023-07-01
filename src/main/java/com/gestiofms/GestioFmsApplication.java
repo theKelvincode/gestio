1947b1ff83e3a750932d8a07abbe86c4e5c70b0e
Kelvin <thekelvincode@gmail.com>
2023-07-01 09:43:37 +0100

    first commit

---
+++ src/main/java/com/gestiofms/GestioFmsApplication.java
@@ -0,0 +1,13 @@
+package com.gestiofms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioFmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioFmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestiofms/GestioFmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiofms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioFmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
