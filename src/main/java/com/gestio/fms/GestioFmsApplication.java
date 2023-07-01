ee5ac3a28b4be17238cd59290fb6d635e5a8359c
Kelvin <thekelvincode@gmail.com>
2023-07-01 14:13:25 +0100

    changes name of package

--- src/main/java/com/gestio/fms/GestioFmsApplication.java
+++ src/main/java/com/gestio/fms/GestioFmsApplication.java
@@ -1,4 +1,4 @@
-package com.gestiofms;
+package com.gestio.fms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gestio/fms/GestioFmsApplicationTests.java
+++ src/test/java/com/gestio/fms/GestioFmsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gestiofms;
+package com.gestio.fms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
